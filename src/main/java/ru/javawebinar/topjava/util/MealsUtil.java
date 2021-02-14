ae20c4d868c20ecb96155a5b1dc2b38527ea290a
Stanislav Popkov <keks1323@yandex.ru>
2021-02-14 10:49:12 +0300

    1 opt 3 HW0 opt2

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MealsUtil {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Meal> meals = Arrays.asList(
                 new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
                 new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
